79eb54581bcbae1f5bc957d163e01b3d9ef28555
ihsan <ihsanzombieihsan@gmail.com>
2024-09-05 15:57:29 +0700

    updated promting

--- templates/prompt.py
+++ templates/prompt.py
@@ -30,6 +30,8 @@ User's CV: {summary} (if relevant)
 """
 
 
+
+
 contextualize_q_system_prompt = """Given a chat history and the latest user question \
 which might reference context in the chat history, formulate a standalone question \
 which can be understood without the chat history. Do NOT answer the question, \
