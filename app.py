5d64ed36854b8f1d98d35596896a7eed8f9d7e5d
ihsan <ihsanzombieihsan@gmail.com>
2024-09-07 13:25:05 +0700

    added new logo

--- app.py
+++ app.py
@@ -10,10 +10,14 @@ from paddleocr import PaddleOCR
 # sess_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
 sess_id = "W56PNA34XM"
 
-# Streamlit page configuration
-st.set_page_config(page_title="Chat Application", layout="wide")
+# Streamlit page configuration with custom logo
+st.set_page_config(
+    page_title="OCBC Job Recommender",
+    layout="wide",
+    page_icon= r"C:\Users\san\Documents\Documents\OCBC\Github_ocbc_workspace\OCBC_CV_RAG\logo.png"  # Replace with the path to your PNG file
+)
 
-# Initialize chat history in session state
+# Rest of your code remains the same
 if "messages" not in st.session_state:
     st.session_state.messages = [
         {"role": "assistant", "content": "Welcome to OCBC's Job Recommender Chatbot! Simply drop your CV below, and we'll instantly analyze it to see if you're a perfect fit for any of our current job openings. Let us help you take the next step in your career with OCBC!"}
@@ -22,82 +26,52 @@ if "messages" not in st.session_state:
 if "current_response" not in st.session_state:
     st.session_state.current_response = ""
 
-# Initialize a state variable for file upload
 if "file_uploaded" not in st.session_state:
     st.session_state.file_uploaded = False
 
-# We loop through each message in the session state and render it as
-# a chat message.
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
 pdf_text = ""
-# Only show the file uploader and link input if no file has been uploaded
+
 if not st.session_state.file_uploaded:
     uploaded_file = st.file_uploader("Upload a PDF", type="pdf")
-    # link_input = st.text_input("Enter a link here")
     
     if uploaded_file is not None:
-    # Process the PDF file
-        # pdf_reader = PyPDF2.PdfReader(uploaded_file)
-        # pdf_text = ""
-        # for page in pdf_reader.pages:
-        #     pdf_text += page.extract_text() 
-        
         ocr_id = PaddleOCR(use_angle_cls=True, lang='en', show_log=False, use_gpu=True)
         pdf_text += extract_text_from_pdf(uploaded_file, ocr_id, 0.8)
         
         st.session_state.messages.append(
-        {"role": "user", "content": pdf_text}
-    )
-
-    # Add our input to the chat window
+            {"role": "user", "content": pdf_text}
+        )
         with st.chat_message("user"):
             st.markdown(pdf_text)
-
         with st.spinner("Thinking ..."):
             response = caller_cv(pdf_text)
-        # Add the response to the session state
         st.session_state.messages.append(
             {"role": "assistant", "content": response}
         )
-
-        # Add the response to the chat window
         with st.chat_message("assistant"):
             st.markdown(response)
-
-        # Optionally, you can display the extracted textssd
         st.write("Extracted text from the PDF:")
         st.text(pdf_text)
         print(pdf_text)
-
-
-        # Set the file_uploaded state to True
         st.session_state.file_uploaded = True
-        st.rerun()  # This will rerun the app and hide the uploader
-
+        st.rerun()
 else:
     st.success("File uploaded successfully!")
-# We take questions/instructions from the chat input to pass to the LLM
-if user_prompt := st.chat_input("Your message here", key="user_input"):
 
-    # Add our input to the session state
+if user_prompt := st.chat_input("Your message here", key="user_input"):
     st.session_state.messages.append(
         {"role": "user", "content": user_prompt}
     )
-
-    # Add our input to the chat window
     with st.chat_message("user"):
         st.markdown(user_prompt)
-
     with st.spinner("Thinking ..."):
         response = caller_question(user_prompt)
-    # Add the response to the session state
     st.session_state.messages.append(
         {"role": "assistant", "content": response}
     )
-
-    # Add the response to the chat window
     with st.chat_message("assistant"):
         st.markdown(response)
